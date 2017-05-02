455b940f88de878c1c2f4f9c7f1aee9596c0f6a4
Alex Indge <woopsire@comcast.net>
2017-05-01 20:41:22 -0600

    Added weights and some training data.

--- neuralnet.py
+++ neuralnet.py
@@ -1,15 +1,42 @@
 import math
-import numpy
+import numpy as np
+import sys
+import random as r
+import matplotlib.pyplot as plt
 
+INPUT_SIZE = 2     # Size of input vector
+OUTPUT_SIZE = 1    # Size of output vector
+BUF_MAX = 0.2        # Maximum size of padding away from 0 and 1 for initial normalized weights
 
-SIGMA = 0
+def get_input_layer():
+    result = []
+    for i in range(INPUT_SIZE):
+        result.append(r.random())
+    return result
+
+def sigmoid(x):
+    return -1 + 2 / (1 + math.exp(-x))
+
+def ddx_sigmoid(x):
+    return 0.5 * (1 + sigmoid(x)) * (1 - sigmoid(x))
+
+def generate_random_weights(layer1_size, layer2_size):
+    '''Generate normalized weights for a given size'''
+    weights = np.random.normal(0, 1, [layer1_size, layer2_size])
+    _min = np.amin(weights)
+    buf = r.random()*BUF_MAX
+    weights = weights - _min
+    weights = weights / np.amax(weights) * (1 - buf * 2)
+    weights = weights + buf
+    return weights
     
-class Neuron(object):
-    def __init__(self, _data=None):
-        self.data = _data
-        self.inputs = [1]
 
-    def output(self):
-        result = 0
-        for i in self.inputs:
-            result += (1-math.exp(-SIGMA * i))/(1 + math.exp(-SIGMA * i))
+if __name__ == "__main__":
+
+    #Generate random initial weights based on a normal distribution
+    hidden_layer_size = int(sys.argv[1])
+    weights = []
+    weights.append(generate_random_weights(INPUT_SIZE, hidden_layer_size))  # Input-to-hidden
+    weights.append(generate_random_weights(hidden_layer_size, OUTPUT_SIZE)) # Hidden-to-output
+    print(weights[0])
+    print(weights[1])
